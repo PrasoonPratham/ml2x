6e0e4356b329659b4e0d9ac44ba55b5c6f394375
Pratham Prasoon <prathamprasoon123@gmail.com>
2021-07-15 10:56:32 +0530

    rounded off prediction

--- model.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-train_data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-test_data = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
-
-model = tf.keras.Sequential([tf.keras.layers.Dense(units=1, input_shape=[1])])
-model.compile(optimizer="sgd", loss="mean_squared_error")
-
-model.fit(train_data, test_data, epochs=500)
